0a7432d791fa0b8ae1d46abb79a20772181b2923
nicknamelb <nickname.lb@yandex.ru>
2024-11-25 10:11:08 +0300

    Добавьте файлы проекта.

---
+++ proga_lab_2/BinaryTree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeProject
+{
+    public class TreeNode
+    {
+        public int Value;
+        public TreeNode? Left;
+        public TreeNode? Right;
+
+        public TreeNode(int value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+    }
+
+    public class BinaryTree
+    {
+        public TreeNode Root;
+
+        public BinaryTree()
+        {
+            Root = null;
+        }
+
+        public int CountLeaves(TreeNode node)
+        {
+            if (node == null)
+                return 0;
+
+            if (node.Left == null && node.Right == null)
+                return 1;
+
+            return CountLeaves(node.Left) + CountLeaves(node.Right);
+        }
+
+        public int GetLeafCount()
+        {
+            return CountLeaves(Root);
+        }
+    }
+}
---
+++ proga_lab_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TreeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+            // Добавление узлов в дерево
+            tree.Root = new TreeNode(1);
+            tree.Root.Left = new TreeNode(2);
+            tree.Root.Right = new TreeNode(3);
+            tree.Root.Left.Left = new TreeNode(4);
+            tree.Root.Left.Right = new TreeNode(5);
+
+            int leafCount = tree.GetLeafCount();
+            Console.WriteLine($"Количество листьев в дереве: {leafCount}");
+        }
+    }
+}
